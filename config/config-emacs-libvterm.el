f6ea22648fb84854ea209765a292f3a79ac33c89
miyatarutyyy <study.miyata026@gmail.com>
2026-02-06 22:11:34 +0900

    perf(vterm): autoload commands instead of requiring at startup

--- config/config-emacs-libvterm.el
+++ config/config-emacs-libvterm.el
@@ -5,7 +5,7 @@
 	     (expand-file-name "config/emacs-libvterm" user-emacs-directory))
 
 (leaf vterm
-  :require t
+  :commands (vterm vterm-other-window)
   :config
   (setq vterm-shell (executable-find "zsh")
 	vterm-max-scrollback 10000))
