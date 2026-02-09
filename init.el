cf325fd03f36ac1cf5de50c12f02584ab9fa1671
miyatarutyyy <study.miyata026@gmail.com>
2026-02-09 21:32:23 +0900

    feat: add TypeScript/TSX grammer sources

--- init.el
+++ init.el
@@ -68,7 +68,13 @@
 ;; always use python tree-sitter
 (add-to-list 'major-mode-remap-alist '(python-mode . python-ts-mode))
 (setq treesit-font-lock-level 4)	     
-
+(with-eval-after-load 'treesit
+  (add-to-list 'treesit-language-source-alist
+	       '(typescript "https://github.com/tree-sitter/tree-sitter-typescript"
+                            "master" "typescript/src"))
+  (add-to-list 'treesit-language-source-alist
+               '(tsx "https://github.com/tree-sitter/tree-sitter-typescript"
+                     "master" "tsx/src")))
 
 ;;; ==========================================================
 ;;; 2) Convenience Functions / Keybind
