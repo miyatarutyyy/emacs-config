f0e8b24ce8212c984fe3161bf1f92e65a49f2327
miyatarutyyy <study.miyata026@gmail.com>
2026-04-24 19:40:01 +0900

    chore: remove all Emacs settings

--- config/config-ace-window.el
+++
@@ -1,9 +0,0 @@
-(leaf ace-window
-  :ensure t
-  :bind (("C-x o" . ace-window))
-  :config
-  (setopt aw-keys '(?a ?s ?d ?f ?g ?h ?i ?j ?k ?l))
-  :custom-face
-  (aw-leading-char-face . '((t (:height 3.0)))))
-
-(provide 'config-ace-window)
